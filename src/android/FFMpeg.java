eb955e1b73fdaa50e61effb274d1c8171b900de9
susheelyadav <97273694+susheelyadav@users.noreply.github.com>
2023-12-07 12:09:52 +0530

    Update FFMpeg.java s

--- src/android/FFMpeg.java
+++ src/android/FFMpeg.java
@@ -16,35 +16,44 @@ import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
  // ref: https://github.com/tanersener/mobile-ffmpeg/wiki/Android
 public class FFMpeg extends CordovaPlugin {
 
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("exec")) {
-            FFmpeg.executeAsync(data.getString(0), new ExecuteCallback() {
+  @Override
+public boolean execute(String action, JSONArray data, CallbackContext callbackContext) {
+    if (action.equals("exec")) {
+        try {
+            String command = data.getString(0);
+            FFmpeg.executeAsync(command, new ExecuteCallback() {
                 @Override
                 public void apply(long executionId, int returnCode) {
                     String result = String.format("Done out=%s", Config.getLastCommandOutput());
-                    if (returnCode == RETURN_CODE_SUCCESS){
+                    if (returnCode == RETURN_CODE_SUCCESS) {
                         callbackContext.success(result);
-                    }
-                    else{
-                    JSONObject errorObject = new JSONObject();
-                    errorObject.put("errorCode", returnCode);
-                    errorObject.put("errorMessage", "Additional error information"); // Add any additional error information if needed
-                    errorObject.put("originalData", data);
-                    callbackContext.error(errorObject);
+                    } else {
+                        JSONObject errorObject = new JSONObject();
+                        errorObject.put("errorCode", returnCode);
+                        errorObject.put("errorMessage", "Additional error information");
+                        errorObject.put("originalData", data);
+                        callbackContext.error(errorObject);
                     }
                 }
             });
             return true;
-        } else if(action.equals("probe")) {
-            MediaInformation info = FFprobe.getMediaInformation(data.getString(0));
-            int returnCode = Config.getLastReturnCode();
-            if(returnCode == RETURN_CODE_SUCCESS) {
-                callbackContext.success(info.getAllProperties());
-            } else {
-                callbackContext.error(Config.getLastCommandOutput());
-            }
-            return true;
-        } else return false;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            callbackContext.error("JSONException: " + e.getMessage());
+            return false;
+        }
+    } else if (action.equals("probe")) {
+        MediaInformation info = FFprobe.getMediaInformation(data.optString(0));
+        int returnCode = Config.getLastReturnCode();
+        if (returnCode == RETURN_CODE_SUCCESS) {
+            callbackContext.success(info.getAllProperties());
+        } else {
+            callbackContext.error(Config.getLastCommandOutput());
+        }
+        return true;
+    } else {
+        return false;
     }
+}
+
 }
